88f7b3e6a487342fa5e7d373654574b9fc285aa1
rpheus <andrea.dutto1994@gmail.com>
2023-11-03 16:36:34 +0100

    Update Lista Prodotti

--- server/aquisti_on_line/src/main/java/com/example/Main.java
+++ server/aquisti_on_line/src/main/java/com/example/Main.java
@@ -25,11 +25,17 @@ public class Main {
             if (!products.isEmpty()) {
                 System.out.println("Prodotti disponibili:");
                 for (Prodotto prodotto : products) {
-                    System.out.println("ID: " + prodotto.getIdProdotto() + " | "
-                        + "Nome: " + prodotto.getNomeProdotto() + " | "
-                        + prodotto.getPrezzoProdotto()+"$ | "
-                        + "Quantità: "+prodotto.getQuantitaProdotto()+ " | "
-                        + "Descrizione:"+prodotto.getDescrizioneProdotto() );
+                    // System.out.println("ID: " + prodotto.getIdProdotto() + " | "
+                    //     + "Nome: " + prodotto.getNomeProdotto() + " | "
+                    //     + prodotto.getPrezzoProdotto()+"$ | "
+                    //     + "Quantità: "+prodotto.getQuantitaProdotto()+ " | "
+                    //     + "Descrizione:"+prodotto.getDescrizioneProdotto() );
+                    System.out.format("-%8s %-20s %-10s %-15s %-20s",  "ID: "+ prodotto.getIdProdotto(),
+                        "| Nome: " + prodotto.getNomeProdotto(),
+                        "| "+ prodotto.getPrezzoProdotto()+"$ ",
+                        "| Quantità: "+prodotto.getQuantitaProdotto(),
+                        "| Descrizione:"+prodotto.getDescrizioneProdotto() );
+                    System.out.println();
                 }
             } else {
                 System.out.println("Nessun prodotto disponibile.");
